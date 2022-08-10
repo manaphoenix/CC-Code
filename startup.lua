75774f7b68c9d97a2b5f02bc5d775cac219784f1
manaphoenix <toehead976@gmail.com>
2022-08-09 22:17:08 -0400

    Updated startup.lua
    Updated colors
    disabled repo-downloader in favor of better system soon:tm:

--- startup.lua
+++ startup.lua
@@ -4,6 +4,78 @@ settings.save()
 term.clear()
 term.setCursorPos(1, 1)
 
+-- intialize var
+local pal = {
+    white = 0xD3D7CF,
+    orange = 0xFF7F00,
+    magenta = 0xFF00FF,
+    lightBlue = 0xADD8E6,
+    yellow = 0xFFD700,
+    lime = 0x00FF00,
+    pink = 0xFFC0CB,
+    gray = 0x1E2227,
+    lightGray = 0x23272E,
+    cyan = 0x529EDC,
+    purple = 0x75507B,
+    blue = 0x0000ff,
+    brown = 0xA52A2A,
+    green = 0x8BC550,
+    red = 0xE42D2D,
+    black = 0x0C0C0C
+}
+
+-- automatic peripheral mounting
+_G.components = {}
+
+-- utility functions
+local function reset()
+    for i, v in pairs(components) do
+        if type(v) == "table" then components[i] = nil end
+    end
+end
+
+local function updateComponents()
+    reset()
+    local perips = peripheral.getNames()
+    if #perips == 0 then return end
+    for _, v in pairs(perips) do
+        local name = peripheral.getType(v)
+        name = name:gsub(".+:(.+)", "%1")
+        if components[name] then
+            local t = 2
+            local test = name .. "_" .. t
+
+            while (components[test] ~= nil) do
+                t = t + 1
+                test = name .. "_" .. t
+            end
+
+            name = test
+        end
+        components[name] = peripheral.wrap(v)
+        components[name].side = v
+    end
+end
+
+-- components metatable
+local componentMT = {
+    __tostring = function(self)
+        -- print all components, formatted
+        local str = ""
+        for k, v in pairs(self) do
+            if type(v) == "table" then
+                str = str .. k .. "\n"
+            end
+        end
+        -- remove extra newline
+        str = str:sub(1, -2)
+        return str
+    end,
+    __call = updateComponents
+}
+
+setmetatable(components, componentMT)
+
 --- RedRun - A very tiny background task runner using the native top-level coroutine
 -- By JackMacWindows
 -- Licensed under CC0, though I'd appreciate it if this notice was left in place.
@@ -33,7 +105,7 @@ function redrun.init(silent)
                         if not ok or coroutine.status(v.coro) ~= "suspended" or v.terminate then delete[#delete + 1] = k end
                     end
                 end
-                for _, v in ipairs(delete) do coroutines[v] = nil end
+                for _, v in pairs(delete) do coroutines[v] = nil end
                 return table.unpack(ev, 1, ev.n)
             end
         }, { __index = os, __isredrun = true })
@@ -44,39 +116,6 @@ function redrun.init(silent)
 end
 
 redrun.init(true)
-
--- utility functions
-local function reset()
-    for i, v in pairs(components) do
-        if type(v) == "table" then components[i] = nil end
-    end
-end
-
--- automatic peripheral mounting
-_G.components = {
-    updateComponents = function()
-        reset()
-        local perips = peripheral.getNames()
-        if #perips == 0 then return end
-        for _, v in ipairs(perips) do
-            local name = peripheral.getType(v)
-            name = name:gsub(".+:(.+)", "%1")
-            if components[name] then
-                local t = 2
-                local test = name .. "_" .. t
-
-                while (components[test] ~= nil) do
-                    t = t + 1
-                    test = name .. "_" .. t
-                end
-
-                name = test
-            end
-            components[name] = peripheral.wrap(v)
-            components[name].side = v
-        end
-    end
-}
 
 -- peripheral watcher
 local function peripheralWatchDog()
@@ -97,50 +136,49 @@ local function peripheralWatchDog()
     end
 end
 
--- intialize var
-local pal = {
-    white = 0xD9D5CC,
-    orange = 0xFB8100,
-    magenta = 0xFF00FF,
-    lightBlue = 0x87CEEB,
-    yellow = 0xC7A000,
-    lime = 0x00ff00,
-    pink = 0xFF69B4,
-    gray = 0x3F3F3F,
-    lightGray = 0x545454,
-    cyan = 0x2BBAA8,
-    purple = 0xD553AB,
-    blue = 0x61AFE3,
-    brown = 0x964B00,
-    green = 0x89CA60,
-    red = 0xcc3300,
-    black = 0x0C0C0C
-}
-
-components.updateComponents()
-
 -- Download Custom Libs
-if http then
+local function downloadFiles()
+    local gitTemplate = "https://raw.githubusercontent.com/manaphoenix/CC_OC-Code/main/"
+    if not http then return end
     local req = http.get("https://api.github.com/repos/manaphoenix/CC_OC-Code/git/trees/main?recursive=1")
-    if req then
-        local gitTemplate = "https://raw.githubusercontent.com/manaphoenix/CC_OC-Code/main/"
-        local files = textutils.unserialiseJSON(req.readAll())
-        req.close()
+    if not req then return end
+    local files = textutils.unserialiseJSON(req.readAll())
+    req.close()
+    local toDownload = {}
 
-        for _, v in pairs(files.tree) do
-            if v.path:match("%.lua") then
-                if not fs.exists(v.path) then
-                    req = http.get(gitTemplate .. v.path)
-                    if req then
-                        local file = fs.open(v.path, "w")
-                        file.write(req.readAll())
-                        file.close()
-                    end
+    local shaFile = fs.open("fileShas.txt", "w")
+    for _, v in pairs(files.tree) do
+        if v.path:match("%.lua") and not fs.exists(v.path) then
+            table.insert(toDownload, function()
+                req = http.get(gitTemplate .. v.path, nil, true)
+                if not req then return end
+                local file = fs.open(v.path, "wb")
+                if file then
+                    file.write(req.readAll())
+                    file.close()
                 end
-            end
+                req.close()
+            end)
         end
+        shaFile.writeLine(v.path .. " " .. v.sha)
     end
+    parallel.waitForAll(table.unpack(toDownload))
+    shaFile.close()
 end
+
+-- cosu auto completion
+-- add completion if it does not exist
+local completions = shell.getCompletionInfo()
+if not completions["cosu.lua"] then
+    local completion = require("cc.shell.completion")
+    local complete = completion.build(
+        completion.file
+    )
+    shell.setCompletionFunction("cosu.lua", complete)
+end
+
+components()
+--downloadFiles()
 
 -- set colors
 for i, v in pairs(pal) do term.setPaletteColor(colors[i], v) end
