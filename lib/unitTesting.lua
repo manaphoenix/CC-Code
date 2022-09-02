53c83a400b2efae2ba101e335ab11de514011d23
manaphoenix <toehead976@gmail.com>
2022-09-02 03:48:35 -0400

    rewrote unitTesting

--- lib/unitTesting.lua
+++ lib/unitTesting.lua
@@ -1,30 +1,64 @@
 -- Unit Testing Library
 local module = {}
 
-local startTime = os.clock()
+local startTime = os.epoch("utc")
+local strs = {
+    start = "Testing: %s",
+    time = "Took: %s"
+}
 
-function module.restart()
-    startTime = os.clock()
+local function nanoToMs(n)
+    local ms = n/1000000
+    ms = math.floor(ms*100)/100
+    return ms
 end
 
-function module.calculate()
-    return os.clock() - startTime
+local function mark()
+    if ccemux then
+        startTime = os.epoch("nano")
+    else
+        startTime = os.epoch("utc")
+    end
 end
 
-function module.startTest(name)
-    print("Starting test: " .. name)
-    module.restart()
+local function getEndTime()
+    if ccemux then
+        return os.epoch("nano") - startTime
+    end
+    return os.epoch("utc") - startTime
 end
 
-function module.endTest(name)
-    print("Ending test: " .. name)
-    print("Time taken: " .. module.calculate())
+local function errHandler(err)
+    print("Unit test failed: " .. err)
 end
 
-function module.doTest(name, func)
-    module.startTest(name)
-    func()
-    module.endTest(name)
+local function calculate()
+    local endTime = getEndTime()
+    local calcTime = ccemux and endTime / 1000000000 or endTime/1000
+    calcTime = math.floor(calcTime)
+    if calcTime > 0 then
+        return os.date("!%X",calcTime) .. "s"
+    elseif endTime > 0 and not ccemux then
+        return endTime .. "ms"
+    elseif nanoToMs(endTime) > 0 and ccemux then
+        return "~" .. nanoToMs(endTime).. "ms"
+    elseif endTime > 0 and ccemux then
+        return endTime .. "ns"
+    else
+        return "Too small to calculate"
+    end
+end
+
+---test how long a function takes to complete
+---@param func function The function to test
+---@param name? string Names the test (Default: unitTest)
+function module.test(func, name)
+    name = name or "unitTest"
+    print(strs.start:format(name))
+    mark()
+    xpcall(func, errHandler)
+    local format = calculate()
+    print(strs.time:format(format))
 end
 
 return module
