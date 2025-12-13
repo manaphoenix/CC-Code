0f317179994c5c15a0e49ae662c26bbc93314f43
manaphoenix <toehead976@gmail.com>
2025-12-13 13:18:10 -0500

    changed rstracker to vsEngine updated vsEngine with more functionality added vsReceiver to the repository

--- apps/vsEngine.lua
+++ apps/vsEngine.lua
@@ -3,6 +3,8 @@ local output_side = "right"
 -- use the peripherals program to determine what its name is ^_^ (it also puts it in chat when you connect via modem too :shrug:)
 local input_side = "left"
 -- side that the input relay is on
+local ender_modem_side = "top"
+-- side that the ender modem is on
 
 local outputsides = { -- overrides the default side mapping, so you can have an input on one side but have it mapped to a different side on the output
     -- valid options are ["top", "bottom", "left", "right", "front", "back"]
@@ -14,14 +16,46 @@ local outputsides = { -- overrides the default side mapping, so you can have an 
     bottom = "bottom" -- bottom is always the bottom
 }
 
+local gearSides = { -- how does each side of the input relay map to the rotation speed controller
+    front = 1,
+    top = 2,
+    left = 3,
+    back = 4
+}
+
+local modemCode = 1337
+-- This is the channel the ender modem operates on (IE. the channel it will receive messages on)
+local securityKey = "dogs"
+-- This is the security key that the ender modem will use to verify messages
+
+local fuelCapacity = 24000
+-- max amount the tank can handle (this has to be hard coded there is no way to detect tank size)
+
+local fuelUpdate = 3 -- how often in seconds should we check the fuel?
+
 --== MAIN CODE (DO NOT MODIFY) ==--
 
 -- constants
 local input_relay = peripheral.wrap(input_side)
 local output_relay = peripheral.wrap(output_side)
+local enderModem = peripheral.wrap(ender_modem_side)
+local stressometer = peripheral.find("Create_Stressometer")
+local speedometer = peripheral.find("Create_Speedometer")
+local tank = peripheral.find("fluid_storage")
+local speedControllers = { peripheral.find("Create_RotationSpeedController") }
 
 assert(input_relay, "Input relay not found on side " .. input_side)
 assert(output_relay, "Output relay not found on side " .. output_side)
+assert(enderModem, "Ender modem not found on side " .. ender_modem_side)
+assert(stressometer, "stressometer not found!")
+assert(speedometer, "speedometer not found!")
+assert(tank, "Fuel tank not found!")
+assert(#speedControllers > 0, "No speed controllers found!")
+
+local running = true -- used to control the main loop
+enderModem.open(modemCode)
+
+local stateFileName = "vsengineState.dat"
 
 -- state
 local lastStates = {
@@ -34,11 +68,8 @@ local lastStates = {
     bottom = false
 } -- for reloading the computer from chunk reloads
 
-term.clear()
-term.setCursorPos(1, 1)
-
 local function saveState()
-    local file = fs.open("rstracker_state.lua", "w")
+    local file = fs.open("data/" .. stateFileName, "w")
     if file then
         file.write(textutils.serialize(lastStates))
         file.close()
@@ -46,7 +77,7 @@ local function saveState()
 end
 
 local function loadState()
-    local file = fs.open("rstracker_state.lua", "r")
+    local file = fs.open("data/" .. stateFileName, "r")
     if file then
         local data = file.readAll()
         file.close()
@@ -79,6 +110,20 @@ local function getInputSides()
     return rtTable
 end
 
+local function sendStateMessage()
+    local data = {
+        key = securityKey,
+        payload = {
+            speed = speedometer.getSpeed(),
+            usedStress = stressometer.getStress(),
+            stressCapacity = stressometer.getStressCapacity(),
+            currentFuel = tank.tanks()[1].amount,
+            capacityFuel = fuelCapacity
+        }
+    }
+    enderModem.transmit(modemCode, modemCode, data)
+end
+
 local function handle_redstone()
     local sides = getInputSides()
 
@@ -96,13 +141,38 @@ local function handle_redstone()
 
     -- Update the output relay
     updateState()
+
+    -- Send a message to the ender modem to update the state
+    sendStateMessage()
+end
+
+local function handleMessage(data)
+    -- TODO: Implement message handling
+end
+
+local function handleTimer()
+    sendStateMessage()
 end
 
 local function handleEvent(evTable)
     local ev = evTable[1]
-    local args = { select(2, unpack(evTable)) } -- not currently used, handler is just setup to allow for more events in the future
+    -- local args = { select(2, unpack(evTable)) } -- not currently used, handler is just setup to allow for more events in the future
     if ev == "redstone" then
         handle_redstone()
+    elseif ev == "key" then
+        local key = evTable[2]
+        if key == keys.x then
+            running = false
+        end
+    elseif ev == "modem_message" then
+        local _, _, _, message, _ = evTable[2], evTable[3], evTable[4], evTable[5], evTable[6]
+        if not message.key or message.key ~= securityKey then
+            return
+        end
+        local data = message.payload
+        handleMessage(data)
+    elseif ev == "timer" then
+        handleTimer()
     end
 end
 
@@ -110,8 +180,18 @@ end
 loadState()
 updateState()
 
-while true do
+term.clear()
+term.setCursorPos(1, 1)
+
+print("VS Engine by Manaphoenix")
+print("Press X to exit")
+
+while running do
+    os.startTimer(fuelUpdate)
     local pull = { os.pullEvent() }
     handleEvent(pull)
-    sleep(0.1) -- to avoid double input detection
+    --sleep(0.1) -- to avoid double input detection
 end
+
+term.clear()
+term.setCursorPos(1, 1)
---
+++ apps/vsReceiver.lua
@@ -0,0 +1,214 @@
+--====================================================================--
+-- CONFIGURATION (EDIT THESE)
+--====================================================================--
+
+--====================================================================--
+-- Peripheral Sides
+--====================================================================--
+-- what side the peripheral is located on
+-- valid options are: "top", "bottom", "left", "right", "front", "back"
+
+local status_monitor_side = "top"   -- Status monitor
+local tuning_monitor_side = "right" -- Tuning monitor
+local ender_modem_side    = "back"  -- Ender modem side
+
+--====================================================================--
+-- Ender Modem Settings
+--====================================================================--
+
+local modemCode           = 1337   -- Channel the ender modem listens on
+local securityKey         = "dogs" -- Security key used to verify messages
+
+--====================================================================--
+-- Text Scaling
+--====================================================================--
+-- Valid range: 0.5 - 5.0 (increments of 0.5 required)
+
+local statusTextScale     = 1.0 -- Status monitor
+local tuningTextScale     = 1.0 -- Tuning monitor
+
+--====================================================================--
+-- Color Overrides
+--====================================================================--
+-- These override the default monitor palette colors.
+
+local statusOverrides     = {
+    gray = 0x171717,
+}
+
+local tuningOverrides     = {}
+
+--====================================================================--
+-- Monitor Colors
+--====================================================================--
+-- These are colors used for different parts of what’s written on the monitor.
+-- Labeled to make them easy to change.
+
+-- Status Monitor
+local statusColors        = {
+    inactive       = colors.orange, -- Inactive gears
+    active         = colors.lime,   -- Active gears
+    fuel           = colors.yellow, -- Fuel indicator
+    stress         = colors.purple, -- Stress indicator
+    speed          = colors.blue,   -- Speed (RPM)
+    refillInactive = colors.gray,   -- Refill label (inactive)
+    refillActive   = colors.red,    -- Refill label (active)
+}
+
+-- Tuning Monitor
+local tuningColors        = {}
+
+--====================================================================--
+--===                    MAIN CODE (DO NOT MODIFY)                 ===--
+--====================================================================--
+
+-- constants
+local enderModem          = peripheral.wrap(ender_modem_side)
+local statusMon           = peripheral.wrap(status_monitor_side)
+local tuningMon           = peripheral.wrap(tuning_monitor_side)
+
+local running             = true -- used to control the main loop
+
+assert(enderModem, "Ender modem not found on side " .. ender_modem_side)
+assert(statusMon, "Status monitor not found on side " .. status_monitor_side)
+assert(tuningMon, "Tuning monitor not found on side " .. tuning_monitor_side)
+
+enderModem.open(modemCode)
+
+statusMon.setTextScale(statusTextScale)
+tuningMon.setTextScale(tuningTextScale)
+
+for colName, col in pairs(statusOverrides) do
+    statusMon.setPaletteColor(colors[colName], col)
+end
+
+for colName, col in pairs(tuningOverrides) do
+    tuningMon.setPaletteColor(colors[colName], col)
+end
+
+local statusConfig = {
+    activeGear = {
+        [1] = false,
+        [2] = false,
+        [3] = true,
+        [4] = false
+    },
+    speed = 0,
+    usedStress = 0,
+    stressCapacity = 1,
+    currentFuel = 1,
+    capacityFuel = 24000
+}
+
+local function writeToMonitor(monitor, text, fg, bg)
+    monitor.setTextColor(fg)
+    monitor.setBackgroundColor(bg)
+    monitor.write(text)
+
+    local cx, cy = monitor.getCursorPos()
+    monitor.setCursorPos(1, cy + 1)
+end
+
+local function updateStatusMonitor()
+    statusMon.clear()
+    statusMon.setCursorPos(1, 1)
+
+    -- Gear
+    for gearNum, state in ipairs(statusConfig.activeGear) do
+        if state then
+            writeToMonitor(statusMon, "[Gear " .. gearNum .. "]", statusColors.active, colors.black)
+        else
+            writeToMonitor(statusMon, "Gear " .. gearNum, statusColors.inactive, colors.black)
+        end
+    end
+
+    -- Fuel
+    statusMon.setCursorPos(1, 6)
+    writeToMonitor(statusMon, "Fuel:  " .. statusConfig.currentFuel .. "/", statusColors.fuel, colors.black)
+    writeToMonitor(statusMon, "       " .. statusConfig.capacityFuel .. " mb", statusColors.fuel, colors.black)
+
+    statusMon.setCursorPos(1, 7)
+    if statusConfig.currentFuel > 0 then
+        writeToMonitor(statusMon, "REFILL", statusColors.refillInactive, colors.black)
+    else
+        writeToMonitor(statusMon, "REFILL", statusColors.refillActive, colors.black)
+    end
+
+    -- Speed
+    statusMon.setCursorPos(10, 1)
+    writeToMonitor(statusMon, "Speed: ", statusColors.speed, colors.black)
+    statusMon.setCursorPos(10, 2)
+    writeToMonitor(statusMon, statusConfig.speed .. " RPM", statusColors.speed, colors.black)
+
+    -- Stress
+    statusMon.setCursorPos(10, 3)
+    writeToMonitor(statusMon, "Stress: ", statusColors.stress, colors.black)
+    statusMon.setCursorPos(10, 4)
+    local stressPercent = math.floor(statusConfig.usedStress / statusConfig.stressCapacity * 100)
+    writeToMonitor(statusMon, stressPercent .. "%", statusColors.stress, colors.black)
+end
+
+local function updateTuningMonitor()
+    -- TODO: program logic for this monitor
+end
+
+local function handleMessage(data)
+    if data.activeGear then
+        for i = 1, 4 do
+            statusConfig.activeGear[i] = data.activeGear[i]
+        end
+    end
+    if data.speed then
+        statusConfig.speed = data.speed
+    end
+    if data.usedStress then
+        statusConfig.usedStress = data.usedStress
+    end
+    if data.stressCapacity then
+        statusConfig.stressCapacity = data.stressCapacity
+    end
+    if data.currentFuel then
+        statusConfig.currentFuel = data.currentFuel
+    end
+    if data.capacityFuel then
+        statusConfig.capacityFuel = data.capacityFuel
+    end
+end
+
+local function handleEvent(event)
+    local ev = event[1]
+    if ev == "key" then
+        local key = event[2]
+        if key == keys.x then
+            running = false
+        end
+    elseif ev == "modem_message" then
+        local _, _, _, message, _ = event[2], event[3], event[4], event[5], event[6]
+        if not message.key or message.key ~= securityKey then
+            return
+        end
+        local data = message.payload
+        handleMessage(data)
+        updateStatusMonitor()
+    end
+end
+
+term.clear()
+term.setCursorPos(1, 1)
+print("VS Receiver by Manaphoenix")
+print("Press X to exit")
+
+-- intial loading of monitors
+updateStatusMonitor()
+updateTuningMonitor()
+
+while running do
+    local ev = { os.pullEvent() }
+    handleEvent(ev)
+end
+
+term.clear()
+term.setCursorPos(1, 1)
+
+statusMon.clear()
+tuningMon.clear()
