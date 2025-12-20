4bbb5624e359df895698b78371307c9454a03852
manaphoenix <toehead976@gmail.com>
2025-12-20 03:50:40 -0500

    fixed themes not using correct structure

--- themes/default.lua
+++ themes/default.lua
@@ -2,20 +2,27 @@
 -- Neutral, readable default theme
 
 return {
-    white     = 0xFFFFFF,
-    orange    = 0xFFA500,
-    magenta   = 0xFF00FF,
-    lightBlue = 0xADD8E6,
-    yellow    = 0xFFFF00,
-    lime      = 0x00FF00,
-    pink      = 0xFFC0CB,
-    gray      = 0x808080,
-    lightGray = 0xD3D3D3,
-    cyan      = 0x00FFFF,
-    purple    = 0x800080,
-    blue      = 0x0000FF,
-    brown     = 0xA52A2A,
-    green     = 0x008000,
-    red       = 0xFF0000,
-    black     = 0x000000
+    meta = {
+        name = "Default",
+        author = "Chat-GPT",
+        description = "Neutral, readable default theme",
+    },
+    colors = {
+        white     = 0xFFFFFF,
+        orange    = 0xFFA500,
+        magenta   = 0xFF00FF,
+        lightBlue = 0xADD8E6,
+        yellow    = 0xFFFF00,
+        lime      = 0x00FF00,
+        pink      = 0xFFC0CB,
+        gray      = 0x808080,
+        lightGray = 0xD3D3D3,
+        cyan      = 0x00FFFF,
+        purple    = 0x800080,
+        blue      = 0x0000FF,
+        brown     = 0xA52A2A,
+        green     = 0x008000,
+        red       = 0xFF0000,
+        black     = 0x000000
+    }
 }
