7df0f94d5faae3ada4bd9def82a637960b4d38b3
manaphoenix <toehead976@gmail.com>
2025-12-24 19:28:11 -0500

    fixed when peripherals gets intialized :eyeroll:

--- apps/vs-app/dashboard/core.lua
+++ apps/vs-app/dashboard/core.lua
@@ -2,24 +2,24 @@ local core = {}
 
 function core.init()
     local config      = require("config.defaults")
-    local display     = require("lib.display")
-    local input       = require("lib.input")
-    local net         = require("lib.net")
     local peripherals = require("lib.peripherals")
-    local protocol    = require("lib.protocol")
-    local state       = require("lib.state")
-    local util        = require("lib.util")
-
-    core.config       = config
-    core.display      = display
-    core.input        = input
-    core.net          = net
-    core.peripherals  = peripherals
-    core.protocol     = protocol
-    core.state        = state
-    core.util         = util
-
     peripherals.init(config)
+    local display    = require("lib.display")
+    local input      = require("lib.input")
+    local net        = require("lib.net")
+    local protocol   = require("lib.protocol")
+    local state      = require("lib.state")
+    local util       = require("lib.util")
+
+    core.config      = config
+    core.display     = display
+    core.input       = input
+    core.net         = net
+    core.peripherals = peripherals
+    core.protocol    = protocol
+    core.state       = state
+    core.util        = util
+
     display.init(config)
 
     return core
