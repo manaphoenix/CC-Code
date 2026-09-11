511c6d8720445a0db77f7e90062c643705367b1c
manaphoenix <toehead976@gmail.com>
2026-09-11 15:57:26 -0400

    update theme to fit cyberdream theme better

--- src/themes/cyberdream.lua
+++ src/themes/cyberdream.lua
@@ -1,26 +1,35 @@
+-- themes/cyberdream.lua
 return {
     meta = {
         name = "CyberDream",
         author = "Manaphoenix",
-        description = "A dark theme with a cyberpunk feel",
-        version = 1.0
+        description = "A dark cyberpunk theme based on the CyberDream WezTerm palette",
+        version = 1.1,
     },
+
     colors = {
+        -- Core
         white     = 0xffffff,
+        black     = 0x16181a,
+
+        -- Indexed colors
         orange    = 0xffbd5e,
-        magenta   = 0xff5ef1,
-        lightBlue = 0x7fc8ff,
-        yellow    = 0xf1ff5e,
-        lime      = 0xb6ff5e,
-        pink      = 0xff5ea0,
-        gray      = 0x3c4048,
-        lightGray = 0x7b8496,
-        cyan      = 0x5ef1ff,
-        purple    = 0xbd5eff,
-        blue      = 0x5ea1ff,
-        brown     = 0xb36a3d,
-        green     = 0x5eff6c,
+
+        -- ANSI / bright ANSI mappings
         red       = 0xff6e5e,
-        black     = 0x16181a
-    }
+        green     = 0x5eff6c,
+        yellow    = 0xf1ff5e,
+        blue      = 0x5ea1ff,
+        magenta   = 0xbd5eff,
+        cyan      = 0x5ef1ff,
+        gray      = 0x3c4048,
+
+        -- CC:Tweaked palette aliases
+        lightGray = 0xffffff,
+        brown     = 0xffbd5e,
+        lime      = 0x5eff6c,
+        pink      = 0xff6e5e,
+        lightBlue = 0x5ea1ff,
+        purple    = 0xbd5eff,
+    },
 }
