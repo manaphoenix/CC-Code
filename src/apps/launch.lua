78a6ce11ae372f7b9507d12348d50dce073bbbe2
manaphoenix <toehead976@gmail.com>
2026-09-13 21:02:16 -0400

    new launcher mockup

--- src/apps/launch.lua
+++ src/apps/launch.lua
@@ -1,3 +1,5 @@
+-- New launcher app
+
 local symbols = require(".lib.core.symbols")
 
 term.clear()
@@ -12,6 +14,8 @@ local function center(text, y)
 end
 
 local function drawButton(w,h)
+    w = math.floor(w)
+    h = math.floor(h)
     -- first line
     local t = "\x97%s"
     term.write(t:format(("\x83"):rep(w-2)))
@@ -48,6 +52,39 @@ local function drawButton(w,h)
     term.setBackgroundColor(bgcolor)
 end
 
+local function makeGrid(rows, columns, width, height, startx, starty)
+    local gridObj = {
+        rows = rows,
+        columns = columns,
+        width = width,
+        height = height,
+        startx = startx,
+        starty = starty,
+        objs = {}
+    }
+    
+    function gridObj:add(obj)
+        table.insert(self.objs, obj)
+        return true
+    end
+    
+    function gridObj:remove(obj)
+        for i,v in pairs(self.objs) do
+            if v == obj then
+                table.remove(self.objs, i)
+                return true
+            end
+        end
+        return false
+    end
+    
+    function gridObj:render()
+        -- TODO
+    end
+    
+    return gridObj
+end
+
 -- Title bar
 term.setBackgroundColor(colors.lightGray)
 term.setTextColor(colors.black)
@@ -66,30 +103,25 @@ term.write(" Apps ")
 
 term.setBackgroundColor(colors.gray)
 term.setTextColor(colors.white)
-term.write(symbols.separator .. " Components " .. symbols.separator .. " PineStore ")
+local t = "| Components | PineStore | Themes | Theme Catalog |"
+t = t:gsub("|",symbols.separator)
+local f = ""
+local b = ""
+for c in t:gmatch(".") do
+    if c == symbols.separator then
+        f = f .. 7
+        b = b .. 0
+    else
+        f = f .. 0
+        b = b .. 7
+    end
+end
+
+term.blit(t,f,b)
 
 -- Work area: rows 3 through my - 1
 -- grid of 3x3 (no idea if this is perm, just trying to get a visual)
-term.setCursorPos(2,4)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx/3+3,4)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx-(mx/3)+3,4)
-drawButton(mx/3-3,my/3-3)
-
-term.setCursorPos(2,my/3+3)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx/3+3,my/3+3)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx-(mx/3)+3,my/3+3)
-drawButton(mx/3-3,my/3-3)
-
-term.setCursorPos(2,my-(my/3)+3)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx/3+3,my-(my/3)+3)
-drawButton(mx/3-3,my/3-3)
-term.setCursorPos(mx-(mx/3)+3,my-(my/3)+3)
-drawButton(mx/3-3,my/3-3)
+local grid = makeGrid(3,3,mx,my-3,1,3)
 
 -- Status bar
 term.setCursorPos(1, my)
@@ -103,7 +135,7 @@ term.setCursorPos(1, my)
 term.setTextColor(colors.black)
 term.write((" Apps: %d/%d Installed"):format(numSystem,numUser))
 term.setCursorPos(mx-#(symbols.filledCircle .. " modem online"),my)
-term.setTextColor(colors.lime)
+term.setTextColor(peripheral.find("modem") and colors.lime or colors.red)
 term.write(symbols.filledCircle .. " modem online")
 term.setTextColor(colors.white)
 
